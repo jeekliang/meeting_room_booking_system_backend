9158d17e4f145b5eaf271989a19e9aef15dc12d8
jxliang <jeekliang@163.com>
2024-11-15 20:54:34 +0800

    feat: 增加 cache-control 中间件

---
+++ src/cache-control.middleware.ts
@@ -0,0 +1,12 @@
+import { Injectable, NestMiddleware } from '@nestjs/common';
+import { Request, Response, NextFunction } from 'express';
+
+@Injectable()
+export class CacheControlMiddleware implements NestMiddleware {
+  use(req: Request, res: Response, next: NextFunction) {
+    res.setHeader('Cache-Control', 'no-cache, no-store, must-revalidate');
+    res.setHeader('Pragma', 'no-cache');
+    res.setHeader('Expires', '0');
+    next();
+  }
+}
